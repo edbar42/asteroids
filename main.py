0147c904008d198b99522a89fc87c56ac43cfa39
Eduardo Barroso <dev@edbar.xyz>
2024-10-29 13:09:27 -0300

    refactor(app): add CircleShape class

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 import pygame
 
+from constants import *
+
 
 def main():
-    print("Starting asteroids!")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill(000000)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
