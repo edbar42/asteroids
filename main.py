60d73b08fb556161f4607a077e515ed09bb1de76
Eduardo Barroso <dev@edbar.xyz>
2024-11-08 23:04:31 -0300

    feat(app): draw player sprite

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pygame
 
-from constants import *
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    playerObj = Player(x, y)
     dt = 0
 
     while True:
@@ -14,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000000)
+        playerObj.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000.0
 
