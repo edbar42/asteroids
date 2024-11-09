67369433c0ebc81299a7d3ec0c0708425653e4d1
Eduardo Barroso <dev@edbar.xyz>
2024-11-08 23:08:08 -0300

    feat(app): add player rotation logic

--- main.py
+++ main.py
@@ -17,10 +17,11 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000.0
         screen.fill(000000)
+        playerObj.update(dt)
         playerObj.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000.0
 
 
 if __name__ == "__main__":
